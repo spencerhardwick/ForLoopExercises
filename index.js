4a798e35740d4cc5e31f344786f89fe939763963
spencerhardwick <spencerh@hey.com>
2021-02-17 18:05:38 +0000

    solved the challenge question

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
     b. Print only the ODD values from 3 - 29, one number per line.
     c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
     d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
+
 for (let i = 0; i < 21; i++) {
   console.log(i);
 }
@@ -13,10 +14,17 @@ for (let i = 3; i < 30; i = i + 2) {
 
 for (let i = 12; i >= -14; i -= 2) {
   console.log(i);
+} 
+
+for (let i = 50; i > 20; i--) {
+  if (i % 3 == 0) {
+    console.log(i);
+  }
 }
 
 /*Exercise #2: 
 Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].
+
 
 
 Construct ``for`` loops to accomplish the following tasks:
